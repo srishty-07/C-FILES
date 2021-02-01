8e0a5e88e0a76a4248eb2f1ad9aba9ff43b20c50
srishty <70388131+srishty-07@users.noreply.github.com>
2021-02-01 16:06:00 +0530

    Add files via upload

---
+++ cfiles.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct node
+{
+int info;
+struct node *link;
+};
+int  main()
+{
+struct node *new, *start;
+int data;
+new= (struct node*)malloc(sizeof(struct node));
+printf("\nEnter the data value to insert in first node\n");
+scanf("%d",&data);
+new->info=data;
+new->link=start;
+start=new;
+return 0;
+}
